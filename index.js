67bea47d955e9832252d0bd804c38c547897c4a3
mohamed-knoud <117742106+mohamed-knoud@users.noreply.github.com>
2023-10-19 13:19:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const smartcar = require('smartcar');
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
+app.use(cors());
 
 const app = express();
 
